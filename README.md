7e38c7cfc792a9af090b10c3928eecc2ec2bfc05
Petrica Martinescu <pmartinescu@gmail.com>
2017-07-30 17:32:21 +0300

    Report github stept status

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Github build notify
 
-A wercker step to notify build status to Github private repo.
-add this step both `build` and `after-build`.
+A wercker step to notify build status for current pipeline to Github private repo.
+add this step to the beginning of the pipeline and to `after-steps` sections of each pipeline that you would like to be reported back to github.
 
 ## Options
 
-### required
+### optional
+
+If not provided the step will pe skipped. Useful for local build.
 
 * `token` - Github oauth token. access https://github.com/settings/applications and generate Personal access token
--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 if [ "$WERCKER_RESULT" = "passed" ]; then
-   export WERCKER_GITHUB_BUILD_NOTIFY_STATE="{\"state\":\"success\",\"target_url\":\"$WERCKER_BUILD_URL\"}";
+   export WERCKER_GITHUB_BUILD_NOTIFY_STATE="{\"state\":\"success\",\"target_url\":\"$WERCKER_BUILD_URL\",\"context\":\"$WERCKER_DEPLOYTARGET_NAME\"}";
 elif [ "$WERCKER_RESULT" = "failed" ]; then
-   export WERCKER_GITHUB_BUILD_NOTIFY_STATE="{\"state\":\"failure\",\"target_url\":\"$WERCKER_BUILD_URL\"}";
+   export WERCKER_GITHUB_BUILD_NOTIFY_STATE="{\"state\":\"failure\",\"target_url\":\"$WERCKER_BUILD_URL\",\"context\":\"$WERCKER_DEPLOYTARGET_NAME\"}";
 else
-   export WERCKER_GITHUB_BUILD_NOTIFY_STATE="{\"state\":\"pending\",\"target_url\":\"$WERCKER_BUILD_URL\"}";
+   export WERCKER_GITHUB_BUILD_NOTIFY_STATE="{\"state\":\"pending\",\"target_url\":\"$WERCKER_BUILD_URL\",\"context\":\"$WERCKER_DEPLOYTARGET_NAME\"}";
 fi
 
 curl -H "Authorization: token $WERCKER_GITHUB_BUILD_NOTIFY_TOKEN" \
