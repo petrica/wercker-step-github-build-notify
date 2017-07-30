9f4852e959a1fc3288a9d47dedc8250a15c08469
Petrica Martinescu <pmartinescu@gmail.com>
2017-07-30 17:38:45 +0300

    Include pipeline step

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 if [ "$WERCKER_RESULT" = "passed" ]; then
-   export WERCKER_GITHUB_BUILD_NOTIFY_STATE="{\"state\":\"success\",\"target_url\":\"$WERCKER_BUILD_URL\",\"context\":\"$WERCKER_DEPLOYTARGET_NAME\"}";
+   export WERCKER_GITHUB_BUILD_NOTIFY_STATE="{\"state\":\"success\",\"target_url\":\"$WERCKER_BUILD_URL\",\"context\":\"wercker/$WERCKER_DEPLOYTARGET_NAME\"}";
 elif [ "$WERCKER_RESULT" = "failed" ]; then
-   export WERCKER_GITHUB_BUILD_NOTIFY_STATE="{\"state\":\"failure\",\"target_url\":\"$WERCKER_BUILD_URL\",\"context\":\"$WERCKER_DEPLOYTARGET_NAME\"}";
+   export WERCKER_GITHUB_BUILD_NOTIFY_STATE="{\"state\":\"failure\",\"target_url\":\"$WERCKER_BUILD_URL\",\"context\":\"wercker/$WERCKER_DEPLOYTARGET_NAME\"}";
 else
-   export WERCKER_GITHUB_BUILD_NOTIFY_STATE="{\"state\":\"pending\",\"target_url\":\"$WERCKER_BUILD_URL\",\"context\":\"$WERCKER_DEPLOYTARGET_NAME\"}";
+   export WERCKER_GITHUB_BUILD_NOTIFY_STATE="{\"state\":\"pending\",\"target_url\":\"$WERCKER_BUILD_URL\",\"context\":\"wercker/$WERCKER_DEPLOYTARGET_NAME\"}";
 fi
 
-curl -H "Authorization: token $WERCKER_GITHUB_BUILD_NOTIFY_TOKEN" \
-    -d $WERCKER_GITHUB_BUILD_NOTIFY_STATE \
-    https://api.github.com/repos/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY/statuses/$WERCKER_GIT_COMMIT
+if [ ! -z "$WERCKER_GITHUB_BUILD_NOTIFY_TOKEN" ]; then
+	curl -H "Authorization: token $WERCKER_GITHUB_BUILD_NOTIFY_TOKEN" \
+	    -d $WERCKER_GITHUB_BUILD_NOTIFY_STATE \
+	    https://api.github.com/repos/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY/statuses/$WERCKER_GIT_COMMIT
+else:
+	info "Github access key not provided. Skipping step."
+fi
